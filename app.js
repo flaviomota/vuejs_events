88d3a2f444c5fe82f974046bf9c351f63393dfd0
Flávio Mota <fmota@student.42lisboa.com>
2023-02-07 13:25:21 +0000

    last changes

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const app = Vue.createApp({
+	data() {
+		return {
+			userInput: '',
+			confirmedInput: '',
+		}
+	},
 	methods: {
 		showAlert() {
 			alert('Button pressed!');
+		},
+		saveInput(event) {
+			this.userInput = event.target.value;
+		},
+		confirmInput() {
+			this.confirmedInput = this.userInput;
 		},
 	}
 });
